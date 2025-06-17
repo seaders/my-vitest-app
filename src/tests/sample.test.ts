9f43e4f52d2007bcf49ab971047c38eae30d45a6
seaders <seaders@users.noreply.github.com>
2025-06-17 13:54:07 +0100

    init

---
+++ src/tests/sample.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "vitest";
+
+describe("custom auth test set", () => {
+  test("ensure we we can test this test", async () => {
+    expect("profile.id").toBe("asdfasdfasdfasdfasdfasdf");
+  });
+});
